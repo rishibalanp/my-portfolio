2f2c7b2bc9da25ac4e3866781434e51cfc2ae35f
Rishibalan <rishibalanp@gmail.com>
2026-03-15 16:46:14 +0530

    changes

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Kj9zBgGvmNvolx1akI2GXubeYgld3OehX3Vz9zdqMd8=\"\n}"
--- .next/dev/static/chunks/app_page_a00e8f85.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4e514e5a._.js",
-  "static/chunks/node_modules_next_3cff121f._.js"
-],
-    source: "dynamic"
-});
